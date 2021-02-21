2018312ff466bfe6c9dd64b1560b7fcd1f3c4113
vedadehhc <51378145+vedadehhc@users.noreply.github.com>
2021-02-21 10:14:07 -0500

    re-organize components into separate folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
-import Home from './Home';
+import Home from './components/Home';
 
 function App() {
   return (
