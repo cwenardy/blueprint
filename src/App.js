cd5ba30800bad6b0f9f36ecfd35114a31d519421
vedadehhc <51378145+vedadehhc@users.noreply.github.com>
2021-02-21 10:04:36 -0500

    fixed package versions and stuff

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
 import React from "react";
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
-import Home from "./Home";
 
-class App extends React.Component {
-  render() {
-    const theme = createMuiTheme({
-      palette: {
-        primary: {
-          main: '#0442a5',
-        },
-        secondary: {
-          main: '#ea6b04',
-        },
-      },
-    });
-    return (
-      <ThemeProvider theme={theme}>
-        <div className="App">
-          <Router>
-            <Switch>
-              <Route path="/" exact component={Home} title="Home" menu="/"/>
-              <Route render={() => <Redirect to="/" />} />
-            </Switch>
-          </Router>
+function App() {return (
+    <div className="home">
+      <div className ="container">
+        <div className="row align-items-center my-5">
+          <div className="col-lg-7">
+            <img
+              className="img-fluid rounded mb-4 mb-lg-0"
+              src="http://placehold.it/900x400"
+              alt=""
+            />
+          </div>
+          <div className="col-lg-5">
+            <h1 className="font-weight-light">Home</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+            
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </div>
         </div>
-      </ThemeProvider>
-    );
-  }
+      </div>
+    </div>
+  );
 }
 
 export default App;
